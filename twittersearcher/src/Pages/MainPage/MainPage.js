e8fffc30a516fb801e3e15fe8ba27a1ebce88204
DouglasFlorido <crascaroth@gmail.com>
2021-03-17 08:52:55 -0300

    Estruturação Basica feita

---
+++ twittersearcher/src/Pages/MainPage/MainPage.js
@@ -0,0 +1,14 @@
+import TweetList from '../../Components/TweetList/TweetList'
+
+
+const MainPage = () =>{
+    return(
+        <>
+        <h1>Twitter Hashtag Searcher!</h1>
+           <input/>
+           <TweetList/>
+        </>
+    )
+}
+
+export default MainPage
